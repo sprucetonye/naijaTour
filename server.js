c2eda862f4651b0ecd9386e661ca27c3b1007305
sprucetonye <waribosamuel@gmail.com>
2023-12-28 13:44:58 +0100

    my first push to the repo

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var app = express();
+
+
+
+app.get('/', function (req, res){
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/profile-picture", function(req, res){
+    var img = fs.readFileSync("profile-1.jpg");
+    res.writeHead(200, {"content-Type": 'image/jpg'});
+    res.end(img, 'binary');
+});
+
+
+app.listen(3000, function(){
+    console.log("app listening on port 300!");
+});
